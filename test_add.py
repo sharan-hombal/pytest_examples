593c05e402261d3aea02650c72c1eca69b238b30
sharan-hombal <sharan1220@gmail.com>
2025-10-14 12:03:47 +0000

    fix unit tests

--- test_add.py
+++ test_add.py
@@ -6,10 +6,10 @@ def test_add_negative():
     assert -4 == -4
 
 def test_add_zero():
-    assert 0 == 1
+    assert (2+0) == 2
 
 def test_equality():
     assert 4 == 4
 
 def test_inequality():
-    assert 4 != 4
+    assert 4 != 2
--- test_calculations.py
+++ test_calculations.py
@@ -6,17 +6,17 @@ from pytest_examples.my_calculations import Calculations
 
 @pytest.fixture
 def instance_input():
-    calc = Calculations(6,4)
+    calc = Calculations(6,2)
     return calc
 
 def test_sum(instance_input):
-    assert instance_input.find_sum() == 10
+    assert instance_input.find_sum() == 8
 
 def test_diff(instance_input):
-    assert instance_input.find_diff() == 2
+    assert instance_input.find_diff() == 4
 
 def test_multiply(instance_input):
-    assert instance_input.find_product() == 24
+    assert instance_input.find_product() == 12
 
 def test_division(instance_input):
-    assert instance_input.find_quotient() == 2  # expected to fail
+    assert instance_input.find_quotient() == 3
